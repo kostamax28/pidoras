4d817d0e49c8dd3510b612d80384bf21c295b463
kostamax28 <96413785+kostamax28@users.noreply.github.com>
2022-08-03 17:13:51 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN="5403069514:AAFHNewQKejzWwjp0vWhmyz-LNG9PNsFnLo" # "" не удалять!
+TOKEN="5519138028:AAGOo7Pl46FjhffsNidiQPVbX_AS-7ONMHw" # "" не удалять!
 
 ADMIN_ID= "5403539045" #айди админа "" и # и текст сотрешь и вставишь свой айди
